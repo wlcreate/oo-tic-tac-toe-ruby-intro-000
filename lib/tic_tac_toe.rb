eb51d3f961358279c620622a695749f3e8ca30f6
Waverley Leung <waverley.nyc@gmail.com>
2020-03-30 20:06:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,8 +61,8 @@ class TicTacToe
         end
   odp ? odp : false
   end
-  def full?(board)
-    board.none?{|index| index == " "}
+  def full?
+    @board.all?{|slot| slot == "X" || slot == "O"}? true : false
   end
   def draw?
     won? ? false : full? ? true : false
@@ -87,5 +87,6 @@ class TicTacToe
     elsif draw?
       puts "Cat's Game!"
     end
+
   end
 end
